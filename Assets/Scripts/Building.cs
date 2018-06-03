2b72eb4ded2a039baad7575c9e84d548e894fb88
a3geek <a3geek@gmail.com>
2018-06-03 17:42:27 +0900

    一旦諦め

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -24,6 +24,23 @@ public class Building : MonoBehaviour
             get { return this.Center.y - this.Size.z * 0.5f; }
         }
 
+        public Vector2 BottomLeft
+        {
+            get { return new Vector2(this.MinX, this.MinZ); }
+        }
+        public Vector2 BottomRight
+        {
+            get { return new Vector2(this.MaxX, this.MinZ); }
+        }
+        public Vector2 TopLeft
+        {
+            get { return new Vector2(this.MinX, this.MaxZ); }
+        }
+        public Vector2 TopRight
+        {
+            get { return new Vector2(this.MaxX, this.MaxZ); }
+        }
+
         public Vector2 Center;
         public Vector3 Size;
 
@@ -31,6 +48,11 @@ public class Building : MonoBehaviour
         public bool IsSuffer(Build other)
         {
             return other.MaxX <= this.MaxX && other.MinX >= this.MinX && other.MaxZ <= this.MaxZ && other.MinZ >= this.MinZ;
+        }
+
+        public bool IsContains(Vector2 point)
+        {
+            return point.x >= this.MinX && point.x <= this.MaxX && point.y >= this.MinZ && point.y <= this.MaxZ;
         }
     }
 
@@ -42,7 +64,6 @@ public class Building : MonoBehaviour
 
     [Space]
     public float mainHeight = 25f;
-
     [Space]
     public List<Build> builds = new List<Build>();
 
@@ -59,72 +80,127 @@ public class Building : MonoBehaviour
             Center = Vector2.zero,
             Size = new Vector3(groundSize.x, this.groundHeight, groundSize.y)
         };
-        this.builds.Add(ground);
-        
-        do
+
+        var first = this.CreateBuild(ground);
+        this.builds.Add(first);
+
+        //var counter = 0;
+        //do
+        //{
+        //    var build = this.CreateBuild(groundSize);
+
+        //    counter++;
+        //    for(var i = 0; i < this.builds.Count; i++)
+        //    {
+        //        var b = this.builds[i];
+
+        //        var min = new Vector2(b.MinX - ground.MinX, b.MinZ - ground.MinZ);
+        //        var max = new Vector2(ground.MaxX - b.MaxX, ground.MaxZ - b.MaxZ);
+
+        //        var isLeft = min.x > max.x;
+        //        var isBototm = min.y > max.y;
+
+        //        var randX = isLeft ? Random.Range(ground.MinX, b.MinX) : Random.Range(b.MaxX, ground.MaxX);
+        //        var randY = isBototm ? Random.Range(ground.MinZ, b.MinZ) : Random.Range(b.MaxZ, ground.MaxZ);
+
+        //        var half = new Vector2(build.Size.x, build.Size.z) * 0.5f;
+        //        build.Center = new Vector2(
+        //            randX - (isLeft ? -half.x : half.x),
+        //            randY - (isBototm ? -half.y : half.y)
+        //        );
+
+        //        bool bl = false, br = false, tl = false, tr = false;
+        //        for(var j = 0; j < this.builds.Count && !(bl & br & tl & tr); j = j + ((j + 1) == i ? 2 : 1))
+        //        {
+        //            bl = bl | this.builds[j].IsContains(build.BottomLeft);
+        //            br = br | this.builds[j].IsContains(build.BottomRight);
+        //            tl = tl | this.builds[j].IsContains(build.TopLeft);
+        //            tr = tr | this.builds[j].IsContains(build.TopRight);
+        //        }
+
+        //        if((bl & br & tl & tr) == false)
+        //        {
+        //            this.builds.Add(build);
+
+
+        //            this.builds.Add(ground);
+        //            return;
+
+
+
+        //            counter = 0;
+        //            break;
+        //        }
+        //    }
+        //}
+        //while(counter < 5);
+
+    }
+
+    private Build CreateBuild(Vector2 groundSize)
+    {
+        var rate = new Vector2(Random.Range(0.5f, 0.75f), Random.Range(0.5f, 0.75f));
+
+        var size = new Vector2(
+            groundSize.x * rate.x,
+            groundSize.y * rate.y
+        );
+
+        return new Build()
         {
-            var rate = new Vector2(Random.Range(0.5f, 0.75f), Random.Range(0.5f, 0.75f));
+            Center = Vector3.zero,
+            Size = new Vector3(size.x, Random.value * this.mainHeight, size.y)
+        };
+    }
 
-            var size = new Vector2(
-                groundSize.x * rate.x,
-                groundSize.y * rate.y
-            );
+    private Build CreateBuild(Build ground)
+    {
+        var build = this.CreateBuild(new Vector2(ground.Size.x, ground.Size.z));
 
-            var half = size * 0.5f;
-            var pos = new Vector2(
-                Random.Range(ground.MinX + half.x, ground.MaxX - half.x),
-                Random.Range(ground.MinZ + half.y, ground.MaxZ - half.y)
-            );
+        var half = new Vector2(build.Size.x, build.Size.z) * 0.5f;
+        build.Center = new Vector2(
+            Random.Range(ground.MinX + half.x, ground.MaxX - half.x),
+            Random.Range(ground.MinZ + half.y, ground.MaxZ - half.y)
+        );
 
-            this.builds.Add(new Build()
-            {
-                Center = pos,
-                Size = new Vector3(size.x, Random.value * this.mainHeight, size.y)
-            });
-
-
-            //var half = size * 0.5f;
-            //var pos = new Vector2(Random.Range(0f, half.x), Random.Range(0f, half.y));
-
-            //pos.x = Mathf.Clamp(pos.x, 0f, size.x);
-            //pos.y = Mathf.Clamp(pos.y, 0f, size.y);
-
-            //this.builds.Add(new Build()
-            //{
-            //    Center = new Vector2(pos.x * (Random.value < 0.5f ? -1 : 1), pos.y * (Random.value < 0.5f ? -1 : 1)),
-            //    Size = new Vector3(size.x, Random.value * this.mainHeight, size.y)
-            //});
-        }
-        while(false);
+        return build;
     }
 
     private void OnDrawGizmos()
     {
-        if(this.builds.Count <= 0)
+        if(this.builds.Count <= 2)
         {
             return;
         }
 
-        for(var i = 0; i < this.builds.Count; i++)
+        this.BuildGizmos(this.builds[0], Color.white);
+        this.BuildGizmos(this.builds[this.builds.Count - 1], Color.red);
+        
+        for(var i = 1; i < this.builds.Count - 1; i++)
         {
-            this.BuildGizmos(this.builds[i]);
+            this.BuildGizmos(this.builds[i], Color.black);
         }
     }
 
-    private void BuildGizmos(Build build)
+    private void BuildGizmos(Build build, Color color)
     {
         var y = build.Size.y * 0.5f;
 
-        Gizmos.color = Color.black;
-        Gizmos.DrawCube(
-            new Vector3(build.Center.x, y, build.Center.y),
-            build.Size
-        );
+        Gizmos.color = color;
+        Gizmos.DrawWireCube(new Vector3(build.Center.x, y, build.Center.y), new Vector3(build.Size.x, 0.1f, build.Size.z));
 
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireCube(
-            new Vector3(build.Center.x, y, build.Center.y),
-            build.Size
-        );
+        //var y = build.Size.y * 0.5f;
+
+        //Gizmos.color = Color.black;
+        //Gizmos.DrawCube(
+        //    new Vector3(build.Center.x, y, build.Center.y),
+        //    build.Size
+        //);
+
+        //Gizmos.color = Color.white;
+        //Gizmos.DrawWireCube(
+        //    new Vector3(build.Center.x, y, build.Center.y),
+        //    build.Size
+        //);
     }
 }
